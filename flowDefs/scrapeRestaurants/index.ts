45a037e6e94973c3d3991fd139d49018f6845095
Jason Holloway <miceeatnicerice@hotmail.com>
2018-07-31 18:39:25 +0100

    tinkering with flowdef

--- flowDefs/scrapeRestaurants/index.ts
+++ flowDefs/scrapeRestaurants/index.ts
@@ -1,16 +1,21 @@
 import Start from "../../core/Start";
 import ManchesterCom from '../../sources/ManchesterCom'
-import _ from 'highland'
-
 
 const scraper = new ManchesterCom();
 
 export default
     (start: Start) =>
-        start(x => scraper.run()
-                        .take(50)
-                        .through(x.save('mancCom2')))
-        .then((x, keys) =>
-            keys.flatMap(k => x.load(k))
-                .reduce(0, (c, r) => c + 1)
-                .tap(c => console.debug('Total', c)))
+        start(x => {
+            const keys = scraper.run()
+                            .take(100)
+                            .through(x.save('mancCom2'));
+            
+            return x.save('keys')(keys);
+        })
+        .then((x, keys) => {
+            console.debug('keys', keys);            //keys here is an array
+                                                    //because it is persisted as data in an object, instead of as a temp value
+            return x.load('keys')
+                    .reduce(0, (c, r) => c + 1)
+                    .tap(c => console.debug('Total', c))
+        });
